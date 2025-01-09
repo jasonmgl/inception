7d7da93629197c8f1f7370970bcbeff96f023994
user <user@user.com>
2025-01-09 20:23:42 +0100

    feat: works with official image of mariadb

--- srcs/requirements/mariadb/script/script.sh
+++ srcs/requirements/mariadb/script/script.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 mkdir -p /run/mysqld
-
-# while ! mysql -u "$DB_USER" -p"$DB_PASS" -h "$DB_HOST" -P "$DB_PORT" -e "SELECT 1;" > /dev/null 2>&1; do
-#     continue;
-# done
-
-service mysqld start
-
 chown -R www-data:www-data /run/mysqld
 
-mysql -e "SET PASSWORD FOR 'root'@'mariadb' = PASSWORD('${DB_ROOT_PASS}');"
-mysql -e "CREATE DATABASE IF NOT EXISTS \'${DB_NAME}\';"
-mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
-mysql -e "GRANT ALL ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u"${DB_ROOT_USER}" -p"${DB_ROOT_PASS}" shutdown
+mysql --version
+
+mysqld_safe &
+
+sleep 15
+
+# mysql -u ${DB_ROOT_USER} -e "SET PASSWORD FOR '${DB_ROOT_USER}'@'%' = PASSWORD('${DB_ROOT_PASS}');"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "CREATE DATABASE IF NOT EXISTS \'${DB_NAME}\';"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+# mysql -u ${DB_ROOT_USER} -p"${DB_ROOT_PASS}" -e "FLUSH PRIVILEGES;"
+
+mysql -u root <<EOF
+
+SET PASSWORD FOR 'root'@'%' = PASSWORD('${DB_ROOT_PASS}');
+ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL ON wordpress.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
 
 exec "$@"
--- srcs/requirements/wordpress/script/script.sh
+++ srcs/requirements/wordpress/script/script.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+sleep 20
+
 API_WP=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
 
 mkdir -p /run/php
-
-chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /run/php
 
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 
-mv /wp-config.php /var/www/html/
-sed -i -e "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-sed -i -e "s/username_here/$DB_ROOT_USER/g" /var/www/html/wp-config.php
-sed -i -e "s/password_here/$DB_ROOT_PASS/g" /var/www/html/wp-config.php
-sed -i -e "s/localhost/$DB_HOST/g" /var/www/html/wp-config.php
-echo "$API_WP" >> /var/www/html/wp-config.php
+chown -R www-data:www-data /var/www/html
 
-./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$DB_ROOT_USER --admin_password=$DB_ROOT_PASS \
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    mv /wp-config.php /var/www/html/
+    sed -i -e "s/database_name_here/$MARIADB_DATABASE/g" /var/www/html/wp-config.php
+    sed -i -e "s/username_here/$MARIADB_USER/g" /var/www/html/wp-config.php
+    sed -i -e "s/password_here/$MARIADB_PASSWORD/g" /var/www/html/wp-config.php
+    sed -i -e "s/localhost/$MARIADB_HOST/g" /var/www/html/wp-config.php
+else
+    echo "WP CONFIG ALREADY EXIST"
+fi
+
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=root --admin_password=$MARIADB_ROOT_PASSWORD \
     --admin_email=jmougel@student.42lyon.fr --allow-root
 
 exec "$@"
