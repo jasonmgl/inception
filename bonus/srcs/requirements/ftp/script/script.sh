dc5943c1012b1ed85d2602bca1373a80e26c0bc5
user <user@user.com>
2025-01-14 19:24:38 +0100

    feat: vsftpd works with a new static website activate

--- bonus/srcs/requirements/ftp/script/script.sh
+++ bonus/srcs/requirements/ftp/script/script.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-if [ ! -f "/etc/ssl/private/vsftpd.pem" ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/ssl/private/vsftpd.pem \
-        -out /etc/ssl/private/vsftpd.pem \
-        -subj "/C=FR/L=Lyon/O=42/OU=student/CN=42"
-fi
-
 mv /etc/vsftpd.conf /etc/vsftpd.conf.ori
+
+adduser --gecos "" $FTP_USER &> /dev/null
+echo "$FTP_USER:$FTP_PASS" | chpasswd &> /dev/null
+chown -R "$FTP_USER:$FTP_USER" /home/$FTP_USER/ftp &> /dev/null
+chmod 777 /home/$FTP_USER/ftp &> /dev/null
+chown "$FTP_USER:$FTP_USER" /home/john/ftp/upload &> /dev/null
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist &> /dev/null
 
 cat <<EOF  > /etc/vsftpd.conf
 listen=YES
 listen_ipv6=NO
 listen_address=0.0.0.0
 listen_port=21
+seccomp_sandbox=NO
+connect_from_port_20=YES
+
+xferlog_enable=YES
+
+ftp_username=$FTP_USER
+local_root=/home/$FTP_USER/ftp
+secure_chroot_dir=/var/run/vsftpd/empty
+allow_writeable_chroot=YES
+chroot_local_user=YES
 
 pasv_enable=YES
-pasv_promiscuous=NO
+pasv_addr_resolve=YES
+pasv_promiscuous=YES
 pasv_min_port=65500
 pasv_max_port=65515
 pasv_address=0.0.0.0
-port_promiscuous=NO
+port_promiscuous=YES
 
 local_enable=YES
 write_enable=YES
@@ -28,12 +39,8 @@ use_localtime=YES
 local_umask=022
 
 ftpd_banner=Welcome guys.
-
-secure_chroot_dir=/var/run/vsftpd/empty
 pam_service_name=vsftpd
 
-# rsa_cert_file=/etc/ssl/private/vsftpd.pem
-# rsa_private_key_file=/etc/ssl/private/vsftpd.pem
 ssl_enable=NO
 ssl_ciphers=HIGH
 ssl_tlsv1=NO
@@ -43,9 +50,13 @@ force_local_data_ssl=NO
 force_local_logins_ssl=NO
 allow_anon_ssl=NO
 
-anonymous_enable=YES
-anon_upload_enable=YES
-anon_mkdir_write_enable=YES
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO
+
+anonymous_enable=NO
+anon_upload_enable=NO
+anon_mkdir_write_enable=NO
 EOF
 
 exec "$@"
